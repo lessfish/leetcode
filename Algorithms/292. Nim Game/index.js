40ba9aec53b8193cf2dc646e75389f0a9256359d
hanzichi <hanzichi@outlook.com>
2016-01-20 19:43:00 +0800

    add

---
+++ Algorithms/292. Nim Game/index.js
@@ -0,0 +1,11 @@
+// Source : https://leetcode.com/problems/nim-game/
+// Author : Han Zichi
+// Date   : 2016-01-20
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var canWinNim = function(n) {
+  return n % 4
+};
