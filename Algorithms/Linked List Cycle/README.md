a3b13002fed76ce89915b8457d9955ad342a97ed
hanzichi <hanzichi@outlook.com>
2016-05-02 22:54:06 +0800

    add Linked List Cycle

---
+++ Algorithms/Linked List Cycle/README.md
@@ -0,0 +1,5 @@
+I just solve it with using extra space.
+
+Traverse the Linked List, mark the node which has been traversed (here use extra space).
+
+It's alse strange that we cannot use window.xx in this code, it's quite strange.
---
+++ Algorithms/Linked List Cycle/linked-list-cycle.js
@@ -0,0 +1,43 @@
+// Source : https://leetcode.com/problems/linked-list-cycle/
+// Author : Han Zichi
+// Date   : 2016-05-02
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+  flag = false;
+
+  dfs(head);
+
+  return flag;
+};
+
+function dfs(node) {
+  if (flag)
+    return;
+
+  if (node === null)
+    return;
+
+  if (node.flag) {
+    flag = true;
+    return;
+  }
+
+  if (node.next === null)
+    return;
+
+  node.flag = true;
+
+  dfs(node.next);
+}
