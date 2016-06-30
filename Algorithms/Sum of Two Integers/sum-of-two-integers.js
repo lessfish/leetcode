5884cc5afcbc61bbe6398739a70bb47207ea2825
hanzichi <hanzichi@outlook.com>
2016-06-30 13:23:41 +0800

    add Sum of Two Integers

---
+++ Algorithms/Sum of Two Integers/sum-of-two-integers.js
@@ -0,0 +1,17 @@
+// Source : https://leetcode.com/problems/sum-of-two-integers/
+// Author : Han Zichi
+// Date   : 2016-06-30
+
+// it's not the right answer according to the description of the question
+// but I really do not want to solve it in another way
+// be wilful!
+// unless you return me with the result of WA
+
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var getSum = function(a, b) {
+  return a + b;
+};
