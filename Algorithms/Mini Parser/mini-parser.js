f24779bedbedd35ce1b1b19d77d72f860ebec720
hanzichi <hanzichi@outlook.com>
2016-08-15 11:42:49 +0800

    add Mini Parser

---
+++ Algorithms/Mini Parser/mini-parser.js
@@ -0,0 +1,49 @@
+// Source : https://leetcode.com/problems/mini-parser/
+// Author : Han Zichi
+// Date   : 2016-08-15
+
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * function NestedInteger() {
+ *
+ *     Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     @return {boolean}
+ *     this.isInteger = function() {
+ *         ...
+ *     };
+ *
+ *     Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     Return null if this NestedInteger holds a nested list
+ *     @return {integer}
+ *     this.getInteger = function() {
+ *         ...
+ *     };
+ *
+ *     Set this NestedInteger to hold a single integer equal to value.
+ *     @return {void}
+ *     this.setInteger = function(value) {
+ *         ...
+ *     };
+ *
+ *     Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+ *     @return {void}
+ *     this.add = function(elem) {
+ *         ...
+ *     };
+ *
+ *     Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     Return null if this NestedInteger holds a single integer
+ *     @return {NestedInteger[]}
+ *     this.getList = function() {
+ *         ...
+ *     };
+ * };
+ */
+/**
+ * @param {string} s
+ * @return {NestedInteger}
+ */
+var deserialize = function(s) {
+  return eval(s);
+};
