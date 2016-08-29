f4a822b180f00fa6a2e84508b6893765c0e3af22
hanzichi <hanzichi@outlook.com>
2016-08-29 13:44:43 +0800

    Add Populating Next Right Pointers in Each Node II

---
+++ Algorithms/Populating Next Right Pointers in Each Node II/bfs-solution.js
@@ -0,0 +1,34 @@
+// Source : https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/
+// Author : Han Zichi
+// Date   : 2016-08-29
+
+/**
+ * Definition for binary tree with next pointer.
+ * function TreeLinkNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = this.next = null;
+ * }
+ */
+
+/**
+ * @param {TreeLinkNode} root
+ * @return {void} Do not return anything, modify tree in-place instead.
+ */
+var connect = function(root) {
+  if (!root)
+    return;
+
+  // use BFS
+  var q = [{node: root, step: 0}];
+
+  while (q.length) {
+    var item = q.shift();
+    var node = item.node;
+    var step = item.step;
+
+    node.left && q.push({node: node.left, step: step + 1});
+    node.right && q.push({node: node.right, step: step + 1});
+
+    q.length && (step === q[0].step) && (node.next = q[0].node);
+  }
+};
