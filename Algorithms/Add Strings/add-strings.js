a8bc80f9167f4ac1b0edc411f67ca563b5b763d3
hanzichi <hanzichi@outlook.com>
2016-10-09 13:54:04 +0800

    Add Add Strings

---
+++ Algorithms/Add Strings/add-strings.js
@@ -0,0 +1,29 @@
+// Source : https://leetcode.com/problems/add-strings/
+// Author : Han Zichi
+// Date   : 2016-10-09
+
+// "use strict";
+
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var addStrings = function(num1, num2) {
+  let [i, j] = [num1.length, num2.length];
+  let ans = '';
+  let add = 0;
+
+  i -= 1, j -= 1;
+  for ( ; i >= 0 || j >= 0; i--, j--) {
+
+    let a = i >= 0 ? +num1[i] : 0;
+    let b = j >= 0 ? +num2[j] : 0;
+    let sum = a + b + add;
+    ans = sum % 10 + ans;
+    add = ~~(sum / 10);
+  }
+
+  add && (ans = add + ans);
+  return ans;
+};
