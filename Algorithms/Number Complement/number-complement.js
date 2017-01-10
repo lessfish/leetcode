c5fbe11cb244b65df7f0a07b17a450cf707cd072
hanzichi <hanzichi@outlook.com>
2017-01-10 16:27:00 +0800

    Number Complement

---
+++ Algorithms/Number Complement/number-complement.js
@@ -0,0 +1,22 @@
+// Source : https://leetcode.com/problems/number-complement/
+// Author : Han Zichi
+// Date   : 2017-01-10
+
+
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var findComplement = function(num) {
+  let ans = 0
+    , add = 1;
+
+  while (num) {
+    if (!(num & 1))
+      ans += add;
+    num >>= 1;
+    add <<= 1;
+  }
+
+  return ans;
+};
