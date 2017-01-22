1c551ff80bbec37eae6a3111496022844bfbab96
hanzichi <hanzichi@outlook.com>
2017-01-22 09:26:39 +0800

    Target Sum

---
+++ Algorithms/Target Sum/dfs-solution.js
@@ -0,0 +1,47 @@
+// Source : https://leetcode.com/problems/target-sum/
+// Author : Han Zichi
+// Date   : 2017-01-22
+
+/**
+ * @param {number[]} nums
+ * @param {number} S
+ * @return {number}
+ */
+var findTargetSumWays = function(nums, S) {
+  let total = 0
+    , len = nums.length;
+
+  for (let item of nums)
+    total += item;
+
+  let target = total - S;
+
+  if (target & 1)
+    return 0;
+
+  target >>= 1;
+
+  // select items from nums whose sum is `target`
+  nums.sort((a, b) => b - a);  // 1000ms
+  // nums.sort((a, b) => a - b);  // 1400ms
+
+  let ans = 0;
+
+  let dfs = (index, sum) => {
+    if (sum === target) {
+      ans++;
+      // consider that nums contains 0
+      // return;
+    }
+
+    if (index === len || sum > target)
+      return;
+
+    for (let i = index; i < len; i++)
+      dfs(i + 1, sum + nums[i]);
+  };
+
+  dfs(0, 0);
+
+  return ans;
+};
