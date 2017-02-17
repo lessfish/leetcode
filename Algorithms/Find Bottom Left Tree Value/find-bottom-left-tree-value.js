4adb80de8442f5cd072be3f1879f5da90453e5fc
hanzichi <hanzichi@outlook.com>
2017-02-17 16:33:26 +0800

    Find Bottom Left Tree Value

---
+++ Algorithms/Find Bottom Left Tree Value/find-bottom-left-tree-value.js
@@ -0,0 +1,29 @@
+// Source : https://leetcode.com/problems/find-bottom-left-tree-value/
+// Author : Han Zichi
+// Date   : 2016-02-17
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var findBottomLeftValue = function(root) {
+  let res = [];
+
+  let dfs = (node, step) => {
+    if (res[step] === undefined)
+      res[step] = node.val;
+
+    node.left && dfs(node.left, step + 1);
+    node.right && dfs(node.right, step + 1);
+  };
+
+  dfs(root, 0);
+  return res.pop();
+};
