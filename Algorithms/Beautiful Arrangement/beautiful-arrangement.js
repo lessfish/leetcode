54f6cb17e0fdbde845915dec1d264eb45158156c
hanzichi <hanzichi@outlook.com>
2017-02-27 17:54:11 +0800

    Beautiful Arrangement

---
+++ Algorithms/Beautiful Arrangement/beautiful-arrangement.js
@@ -0,0 +1,29 @@
+// Source : https://leetcode.com/problems/beautiful-arrangement/?tab=Description
+// Author : Han Zichi
+// Date   : 2016-02-27
+
+/**
+ * @param {number} N
+ * @return {number}
+ */
+var countArrangement = function(N) {
+  let [ans, hash] = [0, {}];
+
+  let dfs = (index) => {
+    if (index === N + 1) {
+      ans++;
+      return;
+    }
+
+    for (let i = 1; i <= N; i++) {
+      if (!hash[i] && (index % i === 0 || i % index === 0)) {
+        hash[i] = true;
+        dfs(index + 1);
+        hash[i] = false;
+      }
+    }
+  };
+
+  dfs(1);
+  return ans;
+};
