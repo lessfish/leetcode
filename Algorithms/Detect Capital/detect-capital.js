76210fb67d43b5b6cff2a836d2fcb3352c880e1f
hanzichi <hanzichi@outlook.com>
2017-02-27 18:01:31 +0800

    Detect Capital

---
+++ Algorithms/Detect Capital/detect-capital.js
@@ -0,0 +1,12 @@
+// Source : https://leetcode.com/problems/detect-capital/?tab=Description
+// Author : Han Zichi
+// Date   : 2016-02-27
+
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+var detectCapitalUse = function(word) {
+  let p = /^([a-z]+|[A-Z]+|[A-Z]{1}[a-z]+)$/;
+  return p.test(word);
+};
